0aed3693293b9b2ea3684559dfb5047b8f022486
nwkami <715190184@qq.com>
2020-10-07 16:30:40 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<xiang />
+</template>
+
+<script>
+import xiang from './components/xiang.vue'
+
+export default {
+    name: 'App',
+    components: {
+        xiang
+    }
+}
+</script>
---
+++ src/components/xiang.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+    刻晴
+</div>
+</template>
