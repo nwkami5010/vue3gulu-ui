4448d70a040959703e008dfb60511ca5ec319330
nwkami <715190184@qq.com>
2020-10-16 17:47:08 +0800

    添加value和input

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,8 +1,27 @@
 <template>
-<div>
-    Switch 组件
-</div>
+<button @click="toggle" :class="{checked: value}"><span></span> </button>
 </template>
+
+<script lang="ts">
+import {
+    ref
+} from 'vue'
+export default {
+    props: {
+        value: Boolean
+    },
+    setup(props, context) {
+        const toggle = () => {
+            context.emit('input', !props.value)
+
+        }
+        return {
+            toggle
+        }
+
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 $h: 22px;
@@ -12,7 +31,7 @@ button {
     height: $h;
     width: $h*2;
     border: none;
-    background: blue;
+    background: grey;
     border-radius: $h/2;
     position: relative;
 }
@@ -25,5 +44,18 @@ span {
     width: $h2;
     background: white;
     border-radius: $h2 / 2;
+    transition: left 250ms;
+}
+
+button.checked {
+    background: blue;
+}
+
+button.checked>span {
+    left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus {
+    outline: none;
 }
 </style>
