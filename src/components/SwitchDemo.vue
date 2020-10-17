6c759f3f529eda845c6b53941ee0a375e9efbde6
nwkami <715190184@qq.com>
2020-10-17 15:47:53 +0800

    创建button组件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,12 +1,19 @@
 <template>
 <div>
-    <Switch />
+    <Switch :value="y" @update:value="y=$event"/>
 </div>
 </template>
+
 <script lang="ts">
+import { ref } from 'vue'
 import Switch from '../lib/Switch.vue'
 export default {
-    components: {Switch}
-    
+    components: {
+        Switch
+    },
+    setup(){
+        const y =ref(false)
+        return {y}
+    }
 }
 </script>
