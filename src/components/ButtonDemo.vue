97d7526d4fe1701132683277c452715024ce6163
nwkami <715190184@qq.com>
2020-10-20 22:05:27 +0800

    添加 click 示例

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,9 +1,12 @@
 <template>
 <div>Button 示例</div>
 <h1>示例1</h1>
-<div><Button theme="button">你好</Button></div>
-<div><Button theme="link">你好</Button></div>
-<div><Button theme="text">你好</Button></div>
+<div>
+    <Button @click="onClick">你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+</div>
 <h1>示例2</h1>
 <div>
     <div>
