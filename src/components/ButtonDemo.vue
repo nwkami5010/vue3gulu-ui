666d56e0ae6c9e2bcdb794cac3a06149e4190399
nwkami <715190184@qq.com>
2020-10-20 21:39:24 +0800

    支持size属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,12 +3,26 @@
   Button 示例
   </div>
   <h1>示例1</h1>
-  <div><Button>你好</Button></div>
+  <div><Button theme="button">你好</Button></div>
+  <div><Button theme="link">你好</Button></div>
+  <div><Button theme="text">你好</Button></div>
+  <h1>示例2</h1>
+  <div>
+    <Button size="big">大大大</Button>
+    <Button>普普通</Button>
+    <Button size="small">小小小</Button>
+  </div>
 </template>
 <script lang="ts">
 import Button from "../lib/Button.vue"
 export default {
-  components:{Button}
+  components:{Button},
+  setup(){
+    const onClick = ()=>{
+
+    }
+    return {onClick}
+  }
   
 }
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,116 @@
 <template>
-<button>
+<button class="gulu-button" :class="classes">
     <slot />
 </button>
 </template>
+
+<script lang="ts">
+import {
+    computed
+} from "vue";
+export default {
+    props: {
+        theme: {
+            type: String,
+            default: "button",
+        },
+        size: {
+            type: String,
+            default: "normal",
+        },
+    },
+    setup(props) {
+        const {
+            theme,
+            size
+        } = props;
+        const classes = computed(() => {
+            return {
+                [`gulu-theme-${theme}`]: theme,
+                [`gulu-size-${size}`]: size,
+            };
+        });
+        return {
+            classes
+        };
+    },
+};
+</script>
+
+<style lang="scss">
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+
+.gulu-button {
+    box-sizing: border-box;
+    height: $h;
+    padding: 0 12px;
+    cursor: pointer;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    background: white;
+    color: $color;
+    border: 1px solid $border-color;
+    border-radius: $radius;
+    box-shadow: 0 1px 0 fade-out(black, 0.95);
+
+    &+& {
+        margin-left: 8px;
+    }
+
+    &:hover,
+    &:focus {
+        color: $blue;
+        border-color: $blue;
+    }
+
+    &:focus {
+        outline: none;
+    }
+
+    &::-moz-focus-inner {
+        border: 0;
+    }
+
+    &.gulu-theme-link {
+        border-color: transparent;
+        box-shadow: none;
+        color: $blue;
+
+        &:hover,
+        &:focus {
+            color: lighten($blue, 10%);
+        }
+    }
+
+    &.gulu-theme-text {
+        border-color: transparent;
+        box-shadow: none;
+        color: inherit;
+
+        &:hover,
+        &:focus {
+            background: darken(white, 5%);
+        }
+    }
+
+    &.gulu-theme-button {
+        &.gulu-size-big {
+            font-size: 24px;
+            height: 48px;
+            padding: 0 16px
+        }
+
+        &.gulu-size-small {
+            font-size: 12px;
+            height: 20px;
+            padding: 0 4px;
+        }
+    }
+}
+</style>
