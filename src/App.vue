9a0b917c9ef365044daa2f43d9c7b675302976f3
nwkami <715190184@qq.com>
2021-05-24 18:11:20 +0800

    修改了localhost空白的bug

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,10 @@
 <template>
-<router-view />
+  <router-view />
 </template>
 
 <script>
-import {
-    provide,
-    ref
-} from 'vue'
-import { router } from './router'
-export default {
-    name: 'App',
-    setup() {
-        const width =document.documentElement.clientWidth
-        const menuVisible = ref(width <= 500 ?false : true)
-        provide('menuVisible', menuVisible) // set
-        router.afterEach(()=>{
-            if(width<= 500){
-                menuVisible.value = false;
-            }
-            
-        });
-    },
 
-};
+export default {
+  name: 'App',
+}
 </script>
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,5 @@
-declare module '*.vue'{
-    import {Component} from 'vue'
-    const component:Component
-    export default component
+declare module '*.vue' {
+  import { ComponentOptions } from 'vue'
+  const componentOptions: ComponentOptions
+  export default componentOptions
 }
