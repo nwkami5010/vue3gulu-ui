388afc6544c0a8cf7dc3190e4c93641e1b8d521a
nwkami <715190184@qq.com>
2021-06-13 18:12:45 +0800

    实现下划线滑动+优化代码

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,38 +1,75 @@
-
 <template>
   <div class="gulu-tabs">
-    <div class="gulu-tabs-nav">
-      <div class="gulu-tabs-nav-item"  v-for="(t,index) in titles"  :key="index">{{t}}</div>
+    <div class="gulu-tabs-nav" ref="container">
+      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
-      <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+      <component :is="current" :key="current.props.title" />
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Tab from './Tab.vue'
+import Tab from './Tab.vue';
+import {
+  computed,
+  ref,
+  onMounted,
+  watchEffect} from 'vue';
+
 export default {
-  props:{
+  props: {
     selected: {
       type: String
     }
   },
   setup(props, context) {
-    const defaults = context.slots.default()
-    defaults.forEach((tag)=> {
-      if(tag.type !== Tab){
-        throw new Error('Tabs子组件类型必须是Tab')
+    const selectedItem = ref < HTMLDivElement > (null)
+    const indicator = ref < HTMLDivElement > (null)
+    const container = ref < HTMLDivElement > (null)
+    onMounted(() => {
+      watchEffect(() => {
+        const {
+          width
+        } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {
+          left: left1
+        } = container.value.getBoundingClientRect()
+        const {
+          left: left2
+        } = selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      })
+    })
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs子组件类型必须是Tab');
       }
-    })
-    const titles = defaults.map((tag)=> {
-      return tag.props.title
-    })
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    const current = computed(() => {
+      return defaults.find(tag => tag.props.title === props.selected)
+      });
 
+    const select = (title: string) => {
+      context.emit('update:selected', title);
+    };
     return {
       defaults,
       titles,
+      current,
+      select,
+      selectedItem,
+      indicator,
+      container
 
-    }
+    };
   }
 
 }
@@ -46,6 +83,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -56,6 +94,15 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
     }
   }
   &-content {
