24741e1b85e5c856bf863c703e32b64687a2f359
nwkami <715190184@qq.com>
2021-07-31 16:48:53 +0800

    features 样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div>
     <div class="topnavAndBanner">
@@ -12,36 +13,86 @@
       </div>
     </div>
     <div class="features">
-      <svg class="icon">
-        <use xlink:href="#icon-vue"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-ts"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-light"></use>
-      </svg>
+      <ul>
+        <li>
+          <svg>
+            <use xlink:href="#icon-vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>骄傲地使用了 Vue 3 Composition API</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 TypeScript </h3>
+          <p>源代码采用 TypeScript 书写（非严格检查）</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-light"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>每个组件的源代码都极其简洁</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
+
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
 export default {
-  components: { Topnav },
+  components: {
+    Topnav
+  },
 };
 </script>
+
 <style lang="scss" scoped>
 $green: #02bcb0;
 $border-radius: 4px;
 $color: #007974;
-.topnavAndBanner{
+.topnavAndBanner {
   background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
   clip-path: ellipse(80% 60% at 50% 40%);
 }
 .features {
-  >svg {
-    width: 64px;
-    height: 64px;
+  margin: 64px auto;
+  width: 400px;
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  >ul {
+    display: flex;
+    flex-wrap: wrap;
+    >li {
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+      >svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+      >h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+      >p {
+        grid-area: text
+      }
+    }
   }
 }
 .banner {
@@ -51,11 +102,10 @@ $color: #007974;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-
-  > .actions{
+  >.actions {
     padding: 8px 0;
     a {
-      margin:0 8px;
+      margin: 0 8px;
       background: $green;
       color: white;
       display: inline-block;
