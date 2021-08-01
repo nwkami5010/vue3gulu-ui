9c31defb027aa5f815fcf1c3846153fb475a8572
nwkami <715190184@qq.com>
2021-08-01 20:17:25 +0800

    复杂一点的高亮

--- src/router.ts
+++ src/router.ts
@@ -7,8 +7,8 @@ import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
 import DocDemo from "./components/DocDemo.vue";
 import Intro from './views/Intro.vue';
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue'
+import GetStarted from './views/GetStarted.vue';
+import Install from './views/Install.vue';
 
 const history = createWebHashHistory();
 export const router = createRouter({
