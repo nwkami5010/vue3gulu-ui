ae6229e25f6df219427de4d5b51f233c046b54fd
nwkami <715190184@qq.com>
2021-08-02 16:49:27 +0800

    重构deploysh，上传github仓库

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,10 @@ git commit -m "update" &&
 
 git branch -M master &&
 
-git remote add origin git@github.com:FrankFang/gulu-ui-website-1.git &&
+git remote add origin git@github.com:nwkami5010/gulu-ui-website-1.git &&
 
 git push -f -u origin master &&
 
 cd -
 
-echo https://fangyinghang.com/gulu-ui-website-1/index.html
+echo
