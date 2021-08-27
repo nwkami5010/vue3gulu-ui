4304975542ea5c7d007e96a20e2c4917cceb9615
nwkami <715190184@qq.com>
2021-08-27 14:39:46 +0800

    feat: add comp grid demo

---
+++ src/components/GridPage/Grid1.demo.vue
@@ -0,0 +1,20 @@
+<demo></demo>
+
+<template>
+
+</template>
+
+<script lang="ts">
+import Row from '../../lib/grid/Row.vue';
+import Col from '../../lib/grid/Col.vue';
+
+export default {
+ components: {
+   Row,Col
+ },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
--- src/components/GridPage/GridDemo.vue
+++ src/components/GridPage/GridDemo.vue
@@ -1,18 +1,29 @@
 <template>
 
-  <div class="row">
-    <div class="col" data-span="2"></div>
-    <div class="col" data-span="22"></div>
+  <div class="exp-content">
+    <Demo :component="Layout1Demo"/>
+    <Demo :component="Layout2Demo"/>
+    <Demo :component="Layout3Demo"/>
+    <Demo :component="Layout4Demo"/>
   </div>
 
 </template>
 
 <script lang="ts">
-
+import Layout1Demo from './Grid1.demo.vue';
+import Layout2Demo from './Layout2.demo.vue';
+import Layout3Demo from './Layout3.demo.vue';
+import Layout4Demo from './Layout4.demo.vue';
+import Demo from '../Demo.vue';
 
 export default {
-
-}
+ components: { Demo },
+  setup(){
+   return {
+     Layout1Demo, Layout2Demo, Layout3Demo, Layout4Demo,
+   };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
