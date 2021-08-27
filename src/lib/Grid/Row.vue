6879de02a4ed43f611ee7264e88deb5bc80059fe
nwkami <715190184@qq.com>
2021-08-27 12:14:07 +0800

    feat: add comp grid row.vue

--- src/lib/Grid/Row.vue
+++ src/lib/Grid/Row.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script lang="ts">
+  import { defineComponent,computed,provide } from 'vue'
   const RowProps = {
     gutter: {
       type: Number,
@@ -20,16 +21,61 @@
       type: String,
       default: 'left',
       validator(val) {
-        return ['top',]
+        return ['top','center','bottom'].includes(val);
       }
+    },
+
+  };
+  export default defineComponent({
+    name: 'KRow',
+    props:RowProps,
+    setup(props){
+      provide('gutter',props.gutter);
+      const rowStyle = computed(()=>{
+        let { gutter } = props;
+        return {
+          marginLeft: `${-gutter! / 2}px`,
+          marginRight: `${-gutter! / 2}px`,
+        };
+      });
+
+      const rowClass = computed(()=> {
+        let { align, verticalAlign} =props;
+        return [
+            align !== 'left' && `k-row-align-${align}`,
+            verticalAlign !== 'top' && `k-row-verticalAlign-${verticalAlign}`
+        ];
+      });
+      return { rowStyle,rowClass};
     }
+  })
 
-  }
-export default {
 
-}
 </script>
 
 <style lang="scss" scoped>
-
+ @import "../../style/var.scss";
+ #{component(row)} {
+   display: flex;
+   flex-wrap: wrap;
+   flex-direction: row;
+   &-align-right {
+     justify-content :end;
+   }
+   &-align-center {
+     justify-content: center;
+   }
+   &-align-spaceBetween {
+     justify-content :space-between;
+   }
+   &-align-spaceAround {
+     justify-content: space-around;
+   }
+   &-verticalAlign-center {
+     align-items: center
+   }
+   &-verticalAlign-bottom {
+     align-items :flex-end
+   }
+ }
 </style>
